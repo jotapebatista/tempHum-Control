9c378c0fd91c4e3c3618678b48561a8cd07311b2
jotapeoliveira <joao-oliveirabatista@hotmail.com>
2022-11-23 23:20:55 +0000

    read temp and stores in influxdb

--- run.py
+++ run.py
@@ -1,7 +1,30 @@
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 import requests
-from datetime import datetime
 import serial
+import random
+import schedule
+import time
 
+bucket = "tempHum"
+
+client = InfluxDBClient(url="http://192.168.30.103:8086", org="BStuff")
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+def writeData():
+  temp = random.randrange(14,36)
+  hum = random.randrange(20,80,10)
+  p = Point("roomTempHum").tag("temp", "room").field("temperature", temp).field("humidity", hum)
+  print(p, temp, hum)
+  write_api.write(bucket=bucket, record=p)
+
+schedule.every(3).seconds.do(writeData)
+
+while 1:
+   schedule.run_pending()
+   time.sleep(1)
+
+'''
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
 }
@@ -10,5 +33,7 @@ params = {
     'db': 'tempHum',
 }
 
-data = 'roomTemp temp=24'
+data = 'roomTemp,temp=24 hum=46'
 response = requests.post('http://192.168.30.103:8086/write', params=params, headers=headers, data=data)
+print("{} {}".format(data, response))
+'''
